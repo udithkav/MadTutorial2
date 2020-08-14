a6e5617c42f9d5419d42341d61edd2713686f9dc
Udith Kavisinghe <it19206660@my.sliit.lk>
2020-08-14 15:19:07 +0530

    Updated Exercise and Fixed errors completed Tutorial

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Life Cycle","OnCreate() Invoked");
+        TextView mmm = findViewById(R.id.TvMsg2);
+        mmm.setText(R.string.Msg2);
+
+
     }
-    TextView mmm = findViewById(R.id.TvMsg2);
-    mmm.setText(R.strings.Msg2);
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Life Cycle", "OnStart Called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Life Cycle", "OnRestart Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Life Cycle", "OnResume Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Life Cycle", "OnPause Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Life Cycle", "OnStop Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Life Cycle", "OnDestroy Called");
+    }
 }
