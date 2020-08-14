b990cb36fc81ad63d312c35aef27bd1dd158c11e
Udith Kavisinghe <it19206660@my.sliit.lk>
2020-08-14 14:53:20 +0530

    Added Exercise 1

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    TextView mmm = findViewById(R.id.TvMsg2);
+    mmm.setText(R.strings.Msg2);
 }
